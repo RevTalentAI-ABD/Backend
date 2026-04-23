3daf7e0f228fe7e8b1966c2dbd9cf9d03c1e8640
Kancharla Brundarika <brundarika.22bce9100@vitapstudent.ac.in>
2026-04-23 16:13:10 +0530

    Added Dependencies

---
+++ revtalent/revtalent/src/main/java/com/revtalent/revtalent/RevtalentApplication.java
@@ -0,0 +1,13 @@
+package com.revtalent.revtalent;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RevtalentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RevtalentApplication.class, args);
+	}
+
+}
---
+++ revtalent/revtalent/src/test/java/com/revtalent/revtalent/RevtalentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.revtalent.revtalent;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RevtalentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
